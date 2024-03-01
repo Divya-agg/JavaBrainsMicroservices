c6cc99a7f9d8498b30017c1b49b6e79b38d45afb
divyaaggarwal01 <divya.aggarwal01@nagarro.com>
2024-03-01 15:51:33 +0530

    Added changes

---
+++ movie-info-service/src/main/java/com/javabrains/movieinfoservice/models/Movie.java
@@ -0,0 +1,42 @@
+package com.javabrains.movieinfoservice.models;
+
+public class Movie {
+
+    private String name;
+    private String movieId;
+    private String description;
+
+    public Movie(String name, String movieId, String description) {
+        this.name = name;
+        this.movieId = movieId;
+        this.description = description;
+    }
+
+    public Movie(){
+
+    }
+    public String getMovieId() {
+        return movieId;
+    }
+
+    public void setMovieId(String movieId) {
+        this.movieId = movieId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+}
